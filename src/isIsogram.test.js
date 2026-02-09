1c3f902b9582ec2419c8bbd4c2439cc2659f541a
Anton Kuchmasov <antek.ukraine@gmail.com>
2026-02-09 15:37:19 +0200

    added tests for isIsogram function

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -1,40 +1,40 @@
-'use strict';
+"use strict";
 
-describe('isIsogram', () => {
-  const { isIsogram } = require('./isIsogram');
+describe("isIsogram", () => {
+  const { isIsogram } = require("./isIsogram");
 
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  it('should return true for all variants of empty string', () => {
-    expect(isIsogram('')).toBe(true);
+  it("should return true for all variants of empty string", () => {
+    expect(isIsogram("")).toBe(true);
   });
 
-  it('should return false for the string with M and m letters', () => {
-    const wordWithTrick = 'MancUtdM';
+  it("should return false for the string with M and m letters", () => {
+    const wordWithTrick = "MancUtdM";
 
     expect(isIsogram(wordWithTrick)).toBe(false);
   });
 
-  it('should return false for `Oops!`', () => {
-    expect(isIsogram(`Oops!`)).toBe(false);
+  it("should return false for `Oops`", () => {
+    expect(isIsogram(`Oops`)).toBe(false);
   });
 
-  it('should return true for `playgrounds`', () => {
+  it("should return true for `playgrounds`", () => {
     const word = `playgrounds`;
 
     expect(isIsogram(word)).toBe(true);
   });
 
-  it('should return false for `look`', () => {
-    const word = 'look';
+  it("should return false for `look`", () => {
+    const word = "look";
 
     expect(isIsogram(word)).toBe(false);
   });
 
-  it('should return false for word `Adam`', () => {
-    const word = 'Adam';
+  it("should return false for word `Adam`", () => {
+    const word = "Adam";
 
     expect(isIsogram(word)).toBe(false);
   });
