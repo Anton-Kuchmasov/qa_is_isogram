966336f04024048b43654536cd2c9fcdc018cf64
Anton Kuchmasov <antek.ukraine@gmail.com>
2026-02-09 15:29:55 +0200

    added tests for isIsogram function

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,18 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should return true for all variants of empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should return false for the string with M and m letters', () => {
+    const wordWithTrick = 'MancUtdM';
+
+    expect(isIsogram(wordWithTrick)).toBe(false);
+  });
+
+  it('should return false for `Oops!`', () => {
+    expect(isIsogram(`Oops!`)).toBe(false);
+  });
 });
